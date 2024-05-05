244f7969b4bbe5761aa94e78ca003b14b4343f18
Shubham Nikam <102355393+Shubz224@users.noreply.github.com>
2024-05-05 19:17:17 +0530

    Setting up Socket.io

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import express from "express";
-
+import path from "path";
 const app = express();
 const PORT = 8000;
 
 //using middlewares 
 
-app.use(express.static("/public"));
+app.use(express.static(path.resolve("./public")));
+
 
 app.get("/",(req,res)=>{
-    res.end("Server Home Page BRo!") ;
+    res.sendFile("/public/index.html") ;
 })
 
 app.listen(PORT,()=> console.log(`Server Running on port : ${PORT} `));
