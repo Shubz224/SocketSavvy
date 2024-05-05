bdeddf61c017d383f066c3efa67aca5267805b26
Shubham Nikam <102355393+Shubz224@users.noreply.github.com>
2024-05-05 19:33:34 +0530

    Connected web sockets from client to server

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 import express from "express";
 import path from "path";
+import { createServer } from 'node:http';
+
+
 const app = express();
 const PORT = 8000;
+import {Server} from "socket.io";
+
+
 
 //using middlewares 
-
 app.use(express.static(path.resolve("./public")));
+
+
+const server = createServer(app);
+const io = new Server(server);
+
+
 
 
 app.get("/",(req,res)=>{
     res.sendFile("/public/index.html") ;
 })
+//socket io
 
-app.listen(PORT,()=> console.log(`Server Running on port : ${PORT} `));
+io.on('connection' , (socket) =>{
+ console.log('a new user has connected ');
+});
+
+
+
+server.listen(PORT,()=> console.log(`Server Running on port : ${PORT} `));
