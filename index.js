711c7bae2ed324c172cb4643cb64f0da57081503
Shubham Nikam <102355393+Shubz224@users.noreply.github.com>
2024-05-05 19:10:29 +0530

    Setting up html static pages

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+import express from "express";
 
+const app = express();
+const PORT = 8000;
+
+//using middlewares 
+
+app.use(express.static("/public"));
+
+app.get("/",(req,res)=>{
+    res.end("Server Home Page BRo!") ;
+})
+
+app.listen(PORT,()=> console.log(`Server Running on port : ${PORT} `));
